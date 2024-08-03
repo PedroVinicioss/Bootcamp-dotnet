c82bdea021e283fd34268b4b18a259bbf075d4c3
PedroVinicioss <110180491+PedroVinicioss@users.noreply.github.com>
2024-08-02 23:55:08 -0300

    Update Aula 02 - Utilizando Classes

--- Aula 02 - Fundamentos/Models/Pessoa.cs
+++ Aula 02 - Fundamentos/Models/Pessoa.cs
@@ -7,8 +7,8 @@ namespace Aula_02___Fundamentos.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
-        public int Idade { get; set; }
+        public string? Nome { get; set; }
+        public int? Idade { get; set; }
 
         public void Apresentar(){
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
--- Aula 02 - Fundamentos/Program.cs
+++ Aula 02 - Fundamentos/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Aula_02___Fundamentos.Models;
+
+Pessoa p = new Pessoa();
+
+p.Nome = "Buta";
+p.Idade = 26;
+p.Apresentar();
